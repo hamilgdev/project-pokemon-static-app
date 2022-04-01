885172ffac2a826a286c9da76c1ea053135e42c7
miltoncodeyt <miltoncodeyt@gmail.com>
2022-04-01 18:10:15 -0500

    feat: added ISG

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -126,15 +126,28 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
     paths: dataStaticPokemon.map((id) => ({
       params: { id }
     })),
-    fallback: false
+    // fallback: false
+    fallback: 'blocking'
   };
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { id } = params as { id: string };
+
+  const pokemon = await getPokemon(id);
+
+  if (!pokemon) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    };
+  }
+
   return {
     props: {
-      pokemon: await getPokemon(id)
+      pokemon
     }
   };
 };
--- utils/getPokemon.ts
+++ utils/getPokemon.ts
@@ -2,12 +2,15 @@ import { pokeApi } from "api";
 import { Pokemon } from "interfaces";
 
 export const getPokemon = async (nameOrId: string) => {
-  const { data } = await pokeApi.get<Pokemon>(`/pokemon/${nameOrId}`);
-  // optimizar data necesaria
-  return {
-    id: data.id,
-    name: data.name,
-    sprites: data.sprites
-  };
-
+  try {
+    const { data } = await pokeApi.get<Pokemon>(`/pokemon/${nameOrId}`);
+    // optimizar data necesaria
+    return {
+      id: data.id,
+      name: data.name,
+      sprites: data.sprites
+    };
+  } catch (error) {
+    return null
+  }
 }
